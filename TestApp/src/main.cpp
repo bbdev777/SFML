a7e1c3a230775d0feaa063d0dc7a7e32d0949a64
alexey <yakovlev.aigo@gazprom-neft.ru>
2024-08-01 23:28:08 +0300

    Update main.cpp

--- TestApp/src/main.cpp
+++ TestApp/src/main.cpp
@@ -11,8 +11,8 @@ enum struct ResultCodes : int
 
 int SfmlWindow()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+    sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
