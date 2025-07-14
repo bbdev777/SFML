916e51988a7cefc2cec9ab8b1ee0d85be9dfc8fe
Alexey Yakovlev <yakovlev.aigo@gazprom-neft.ru>
2025-07-14 14:15:28 +0300

    Сборка не из vcpkg

---
+++ Stars/build_script/run_cmake_sys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Скрипт проверки сборки вне VSCode и прочих редакторов
+
+#PREFIX_PATH="/opt/Qt6.4.3/6.4.3/gcc_64;/opt/Qt5.12.3/5.12.3/gcc_64"
+
+start=$(date +%s)
+
+cd ../
+mkdir ./build
+cd ./build
+cmake ../ -DCMAKE_BUILD_TYPE=Release 
+make -j1
+
+end=$(date +%s)
+
+echo Build time was  $(($end-$start)) seconds.
+
